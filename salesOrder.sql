527804c1c2b367e840e2798ca63229f3bf9afd04
mamzycr7 <davidmahmud664@gmail.com>
2024-07-16 19:20:59 +0100

    the updated files

--- salesOrder.sql
+++ salesOrder.sql
@@ -1,7 +1,7 @@
 USE [InventoryMgt]
 GO
 
-/****** Object:  Table [dbo].[salesOrder]    Script Date: 7/12/2024 9:57:24 PM ******/
+/****** Object:  Table [dbo].[salesOrder]    Script Date: 7/16/2024 7:19:11 PM ******/
 SET ANSI_NULLS ON
 GO
 
@@ -10,12 +10,11 @@ GO
 
 CREATE TABLE [dbo].[salesOrder](
 	[salesId] [int] NOT NULL,
-	[date] [date] NOT NULL,
-	[time] [datetime] NOT NULL,
-	[customerId] [int] NOT NULL,
-	[amount] [float] NOT NULL,
-	[status] [text] NOT NULL,
- CONSTRAINT [PK_salesOrder] PRIMARY KEY CLUSTERED 
+	[date] [date] NULL,
+	[customerId] [int] NULL,
+	[total] [float] NULL,
+	[status] [text] NULL,
+PRIMARY KEY CLUSTERED 
 (
 	[salesId] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
--- salesOrderDetail.sql
+++ salesOrderDetail.sql
@@ -1,7 +1,7 @@
 USE [InventoryMgt]
 GO
 
-/****** Object:  Table [dbo].[salesOrderDetail]    Script Date: 7/12/2024 9:58:55 PM ******/
+/****** Object:  Table [dbo].[salesOrderDetail]    Script Date: 7/16/2024 7:19:32 PM ******/
 SET ANSI_NULLS ON
 GO
 
@@ -10,11 +10,11 @@ GO
 
 CREATE TABLE [dbo].[salesOrderDetail](
 	[itemId] [int] NOT NULL,
-	[product] [nvarchar](50) NOT NULL,
-	[quantity] [int] NOT NULL,
-	[unitPrice] [float] NOT NULL,
-	[totalPrice] [float] NOT NULL,
- CONSTRAINT [PK_salesOrderDetail] PRIMARY KEY CLUSTERED 
+	[productName] [nvarchar](1) NULL,
+	[quantity] [int] NULL,
+	[unitPrice] [float] NULL,
+	[totalPrice] [float] NULL,
+PRIMARY KEY CLUSTERED 
 (
 	[itemId] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
